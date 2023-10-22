9fb3a0beb4367dc0e4b28b5045fc68a8c70e8129
mrrsmburu <mrrsmburu@gmail.com>
2023-10-22 12:11:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 
-function addingEventListener() {
-    const input = document.querySelector('#myInput');
-    
-
-    input.addEventListener('click', function() {
-      console.log('Input element clicked!');
-    });
-  }
-  
-
-  addingEventListener();
